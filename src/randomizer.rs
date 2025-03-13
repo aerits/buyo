8deac6e96d86f689776879f0f3eaab2171120066
aerits <stev_nm@protonmail.com>
2025-03-13 12:45:42 -0600

    Fixed all todos

--- src/randomizer.rs
+++ src/randomizer.rs
@@ -1,20 +1,26 @@
-use std::thread::current;
+use rand::{rngs::ThreadRng, Rng};
 
 pub struct Randomizer {
     queue: Vec<i32>,
     max: i32,
-    current: i32
+    current: i32,
+    rng: ThreadRng
 }
 
 impl Randomizer {
     pub fn new(max: i32) -> Randomizer {
-        Randomizer { queue: Vec::new(), max, current: 0 }
+        let rng = rand::rng();
+        Randomizer { queue: Vec::new(), max, current: 0, rng }
     }
     pub fn get(&mut self, i: i32) -> i32 {
-        if (self.queue.len() as i32) < i {
-            // TODO
+        while (self.queue.len() as i32) < i+1 {
+            let new_num = self.rng.random_range(0..self.max);
+            self.queue.push(new_num);
         }
-        return 0;
+        return *self.queue.get(i as usize).unwrap();
+    }
+    pub fn current_pointer(&self) -> i32 {
+        return *&self.current
     }
     pub fn next(&mut self) -> i32 {
         let a = self.get(self.current);
